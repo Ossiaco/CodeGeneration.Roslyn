ea032ab01a6b14c62ac7131f13bd8182c4692888
Richard Fuoco <richf@ossiaco.com>
2019-11-03 09:41:48 -0500

    added IJsonSerializeable interface

---
+++ src/CodeGeneration.Chorus.Attributes/IJsonSerializeable.cs
@@ -0,0 +1,20 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Ossiaco Inc.  All rights reserved.
+//------------------------------------------------------------
+
+namespace CodeGeneration.Chorus.Attributes
+{
+    using System.Text.Json;
+
+    /// <summary>
+    /// Defines the <see cref="IJsonSerializeable" />.
+    /// </summary>
+    public interface IJsonSerializeable
+    {
+        /// <summary>
+        /// The ToJson
+        /// </summary>
+        /// <param name="writer">The writer<see cref="Utf8JsonWriter"/>.</param>
+        void ToJson(Utf8JsonWriter writer);
+    }
+}
